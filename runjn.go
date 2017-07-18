548f099882883e268db28bcd359ba658d30ff258
Wataru NAKAI <watnk0806@gmail.com>
2017-07-18 14:36:48 +0900

    MacOS, pyenv

--- runjn.go
+++ runjn.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/url"
 	"os"
@@ -13,6 +14,8 @@ import (
 
 	"path"
 
+	"runtime"
+
 	"github.com/mitchellh/go-homedir"
 	"github.com/skratchdot/open-golang/open"
 )
@@ -20,8 +23,28 @@ import (
 func normpath(p string) string {
 	return strings.Replace(p, "\\", "/", -1)
 }
+
+func isExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return err == nil
+}
+
 func main() {
 	home, _ := homedir.Dir()
+	if runtime.GOOS == "darwin" {
+		// Use pyenv?
+		pyenv := filepath.Join(home, ".pyenv", "version")
+		if isExist(pyenv) {
+			// pyenv global version
+			data, err := ioutil.ReadFile(pyenv)
+			if err != nil {
+				log.Fatal(err)
+			}
+			pyenv_version := strings.TrimSpace(string(data))
+			os.Setenv("PYENV_VERSION", pyenv_version)
+			defer os.Unsetenv("PYENV_VERSION")
+		}
+	}
 	jupyter, err := exec.LookPath("jupyter")
 	if err != nil {
 		log.Fatal(err)
